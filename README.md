ad145e5b214f9522ce17378739e0493c06cb58ee
Marcelo Nogueira <marcelo.bojikian@gmail.com>
2024-04-05 19:39:28 +0100

    Add installer and update the README

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# homelab
+
+<h1 align="center">
+    Homalab Scripts v1.0
+</h1>
+
+<p align="center">
+  <img alt="License" src="https://img.shields.io/static/v1?label=license&message=MIT">
+</p>
+
+<h4 align="center">
+  Scripts designed to install project and make the first setup.
+</h4>
+
+<p align="center">
+
+  <a href="https://github.com/marcelobojikian" target="_blank">
+    <img alt="Made by Marcelo Nogueira" src="https://img.shields.io/badge/Feito%20por-Marcelo_Nogueira-informational">
+  </a>
+  <a href="https://github.com/marcelobojikian" target="_blank" >
+    <img alt="Github - Marcelo Nogueira" src="https://img.shields.io/badge/Github--%23F8952D?style=social&logo=github">
+  </a>
+  <a href="https://www.linkedin.com/in/marcelobojikian/" target="_blank" >
+    <img alt="Linkedin - Marcelo Nogueira" src="https://img.shields.io/badge/Linkedin--%23F8952D?style=social&logo=linkedin">
+  </a>
+
+</p>
+
+<p align="center">
+  <a href="#-requirements">Requirements</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-how-to-use">How to use</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#memo-licença">Licença</a>
+</p>
+
+<br>
+
+## 🔖 Requirements
+
+To use this script, make sure the packages are installed.
+
+ - curl
+ - python3
+ - git
+ - ssh-keygen
+
+## 🤔 How to use
+
+1. In the terminal,
+    ```bash
+    bash -c "$(curl -sSL https://github.com/marcelobojikian/homelab/raw/main/install.sh)"
+    ```
+
+## :memo: Licença
+
+This project is under the MIT licence. See the [LICENSE](LICENSE) file for more details.
---
+++ install.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+##########################################################################
+# Shellscript:	synopsis - Installs and prepare the project
+# Author     :	Marcelo Nogueira <marcelo.bojikian@gmail.com>
+# Date       :	2024-04-05
+# Category   :	System Installer
+##########################################################################
+# Reference
+# Gene       : https://github.com/marcelobojikian/homelab
+##########################################################################
+# Description
+#   Download and Install the project to configure the proxmox.
+#
+##########################################################################
+
+################################################# variables
+readonly PROJECT_ROOT=https://github.com/marcelobojikian/homelab.git
+###########################################################
+
+############################################## dependencies
+for check in 'python3' 'git' 'ssh-keygen' ; do
+    if ! which "$check" &> /dev/null
+        then echo "Must have $check installed" && exit 1
+    fi
+done
+###########################################################
+
+################################################# functions
+
+log() { msg="${@:2}"; printf "$(date "+%Y-%m-%d %H:%M:%S") - [%s] - %s\n" "${1^^}" "${msg}"; }
+
+setup() {
+
+    sudo apt update -y
+    sudo apt install python3-venv -y
+    sudo apt install python3-pip -y
+
+}
+
+download() {
+    git clone $PROJECT_ROOT
+}
+
+install(){
+
+    cd homelab
+
+    python3 -m venv venv
+    source venv/bin/activate
+
+    pip install ansible-core
+
+    cd src/admin
+
+    ansible-playbook setup.yml
+    
+    deactivate
+}
+###########################################################
+
+################################################# Principal
+log info Instalando Home Lab 
+setup
+download
+install
+log info Instalacao completa
